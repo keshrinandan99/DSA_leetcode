fa01e5a1e91551a5ac55f026cf4851244d900b82
Keshri Nandan <133596161+keshrinandan99@users.noreply.github.com>
2026-01-31 01:52:54 +0530

    Time: 30 ms (31.99%) | Memory: 13 MB (38.70%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int t[1001][1001];
+    int solve(string &s, int i,int j){
+        if(i>=j)return 1;
+        if(t[i][j]!=-1)return t[i][j];
+        if(s[i]==s[j])return t[i][j]= solve(s,i+1,j-1);
+        return t[i][j]=0;
+    }
+    int countSubstrings(string s) {
+        int ans=0;
+        memset(t,-1,sizeof(t));
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                if(solve(s,i,j)){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
